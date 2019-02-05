a5860d8652c5ed1928c75f659f390e93ed1bd614
Luke Turner <turner.luke@outlook.com>
2019-02-05 07:40:30 +0100

    Add project files.

---
+++ kama/kama/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using kama.Models;
+
+namespace kama.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
